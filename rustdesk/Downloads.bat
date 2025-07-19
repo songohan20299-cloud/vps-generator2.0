1d3be785ece4162699844eaa97f8ba687711f4c4
songohan20299-cloud <songohan20299@gmail.com>
2025-07-19 20:46:03 -0300

    Update Downloads.bat

--- rustdesk/Downloads.bat
+++ rustdesk/Downloads.bat
@@ -1,10 +1,10 @@
 @echo off
 choco install anydesk -y --ignore-checksums --force --no-progress --pre
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/songohan20299-cloud/vps-generator2.0/refs/heads/main/Start.bat -OutFile start.bat"
+powershell -Command "Invoke-WebRequest https://gitlab.com/chamod12/anydesk-windows-rdp/-/raw/main/start.bat -OutFile start.bat"
 pip install pyautogui --quiet
 pip install psutil --quiet
-curl -s -L -o time.py https://raw.githubusercontent.com/songohan20299-cloud/vps-generator2.0/main/rustdesk/timelimit.py
-curl -s -L -o shutdown.bat https://raw.githubusercontent.com/songohan20299-cloud/vps-generator2.0/refs/heads/main/rustdesk/loop.bat
+curl -s -L -o timelimit.js https://gitlab.com/chamod12/anydesk-windows-rdp/-/raw/main/timelimit.js
+curl -s -L -o time.py https://gitlab.com/chamod12/anydesk-windows-rdp/-/raw/main/timelimit.py
 curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
 curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
 powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
@@ -14,7 +14,5 @@ C:\Users\Public\Desktop\Winrar.exe /S
 del C:\Users\Public\Desktop\Winrar.exe
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > errormsg.txt 2>&1
 del /f "C:\Users\Public\Desktop\Unity Hub.lnk" > errormsg.txt 2>&1
-set password=@#Disala123456
-powershell -Command "Set-LocalUser -Name 'runneradmin' -Password (ConvertTo-SecureString -AsPlainText '%password%' -Force)"
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d 0 /f
 tzutil /s "Sri Lanka Standard Time"
