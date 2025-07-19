1fc5dc1352bfbec8871156c7915a08cab749b961
songohan20299-cloud <songohan20299@gmail.com>
2025-07-19 20:32:10 -0300

    Update Downloads.bat

--- rustdesk/Downloads.bat
+++ rustdesk/Downloads.bat
@@ -1,11 +1,10 @@
 @echo off
-curl -L -o login.py https://raw.githubusercontent.com/katakishi0/vps-generator2.0/main/rustdesk/login.py
-curl -L -o loop.bat https://raw.githubusercontent.com/katakishi0/vps-generator2.0/main/rustdesk/loop.bat
-curl -L -o show.bat https://raw.githubusercontent.com/katakishi0/vps-generator2.0/main/rustdesk/show.bat
-certutil -urlcache -split -f "https://github.com/rustdesk/rustdesk/releases/download/1.2.1/rustdesk-1.2.1-x86_64.exe" rustdesk.exe
+choco install anydesk -y --ignore-checksums --force --no-progress --pre
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/songohan20299-cloud/vps-generator2.0/refs/heads/main/Start.bat -OutFile start.bat"
 pip install pyautogui --quiet
 pip install psutil --quiet
-curl -s -L -o time.py https://raw.githubusercontent.com/katakishi0/vps-generator2.0/main/rustdesk/timelimit.py
+curl -s -L -o time.py https://raw.githubusercontent.com/songohan20299-cloud/vps-generator2.0/main/rustdesk/timelimit.py
+curl -s -L -o shutdown.bat https://raw.githubusercontent.com/songohan20299-cloud/vps-generator2.0/refs/heads/main/rustdesk/loop.bat
 curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
 curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
 powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
@@ -17,7 +16,5 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > errormsg.txt 2>&1
 del /f "C:\Users\Public\Desktop\Unity Hub.lnk" > errormsg.txt 2>&1
 set password=@#Disala123456
 powershell -Command "Set-LocalUser -Name 'runneradmin' -Password (ConvertTo-SecureString -AsPlainText '%password%' -Force)"
-start "" "rustdesk.exe"
-python login.py
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d 0 /f
 tzutil /s "Sri Lanka Standard Time"
